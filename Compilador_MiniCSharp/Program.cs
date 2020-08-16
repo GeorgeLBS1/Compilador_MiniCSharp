d1cb9b67f25924f899357f2fa6ddd6732467636b
Diaz Monterroso <jj.diaz002@gmail.com>
2020-08-16 16:28:39 -0600

    Creación del proyecto

---
+++ Compilador_MiniCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Compilador_MiniCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
