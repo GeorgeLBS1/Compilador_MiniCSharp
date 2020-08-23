8ce773ecbd3a7c61d03ae06c5529c1af61cf03a8
marcelo rosales <mgrt99@gmail.com>
2020-08-23 17:43:16 -0600

    metodo de lectura

--- Compilador_MiniCSharp/AnalizadorLexico.cs
+++ Compilador_MiniCSharp/AnalizadorLexico.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.ComponentModel.Design;
+using System.Linq;
 
 namespace Compilador_MiniCSharp
 {
     public class AnalizadorLexico
     {
+        public static List<string> Comentarios = new List<string>(); //Se guardan los comentarios
         List<string> ER = new List<string>() //Lista que contiene todas las expresiones regulares a usar para analizar el archivo y la sintaxis de este
         {
             @"^void$|^int$|^double$|^bool$|^string$|^class$|^const$|^interface$|^null$|^this$|^for$|^while$|^foreach$|^if$|^else$|^return$|^break$|^New$|^NewArray$|^Console$|^WriteLine$", //Palabras reservadas, 0
@@ -33,20 +37,124 @@ namespace Compilador_MiniCSharp
             return -100; //Valor negativo asignado si no concuerda ninguna expresión regular
         }
 
+
         public void LeerArchivo(string path) //Lectura del archivo completo
         {
             StreamReader reader = new StreamReader(path);
-            List<string> ArchivoDepurado = new List<string>(); //Archivo que no contendrá tabulaciones 
-            List<string> ArchivoTalCual = new List<string>(); //Archivo como existe a nivel lógico, sin ninguna edición
+
             string Linea = string.Empty; //Variable para ayudar a la lectura del archivo linea a linea
+            bool CierreComentario; //verifica si existe cierre de comentario
+            int ContadorLineas = 0;
 
             while ((Linea = reader.ReadLine()) != null) //Leer todas las lineas del archivo 
             {
-                ArchivoTalCual.Add(Linea); //Añadir linea nítida
-                Linea = Linea.Replace("\t",""); //Se quitan las tabulaciones
-                ArchivoDepurado.Add(Linea); //Se añade al archivo depurado
+                CierreComentario = false;
+                if (!Linea.Contains("/*")) //No trae comentario
+                {
+                    if (Linea.Equals(""))
+                    {
+                        ContadorLineas++;
+                    }
+                    else
+                    {
+                        Linea = Linea.Replace("\t", " "); //cambiar tabulaciones por espacio
+                        Analisis(Linea, ContadorLineas);   //enviar al analizador
+                    }
+
+                }
+                else
+                {
+                    if (!Linea.Contains("*/"))
+                    {
+                        var Divisior = Linea.Split("/*", StringSplitOptions.RemoveEmptyEntries);
+                        bool comprobar = false;
+                        if (Divisior.Length > 1)
+                        {
+                            comprobar = true;
+                        }
+                        if (comprobar == true)
+                        {
+                            Analisis(Divisior[0], ContadorLineas);
+                            Comentarios.Add("/*" + Divisior[1]);
+                            Linea = reader.ReadLine();
+                            ContadorLineas++;
+
+                        }
+
+                        while (!Linea.Contains("*/"))
+                        {
+
+                            Comentarios.Add(Linea);
+                            Linea = reader.ReadLine();
+                            ContadorLineas++;
+
+                            if (reader.EndOfStream && Linea == null)
+                            {
+                                CierreComentario = true;
+                                break;
+
+                            }
+
+
+                        }
+
+                    }
+                    if (CierreComentario == false)
+                    {
+                        var DividirComentario = Linea.Split("*/", StringSplitOptions.RemoveEmptyEntries);
+                        if (DividirComentario.Length == 0)
+                        {
+                            Comentarios.Add("*/");
+                        }
+                        if (DividirComentario.Length == 1)
+                        {
+                            Comentarios.Add(DividirComentario[0] + "*/");
+                        }
+
+                        if (DividirComentario.Length > 1)
+                        {
+
+                            if (DividirComentario[0].Contains("/*"))
+                            {
+                                var Divisor = DividirComentario[0].Split("/*", StringSplitOptions.RemoveEmptyEntries);
+                                if (Divisor.Length > 1)
+                                {
+                                    Analisis(Divisor[0], ContadorLineas);
+
+                                    if (DividirComentario.Length > 1)
+                                    {
+                                        Analisis(DividirComentario[1], ContadorLineas);
+                                        Comentarios.Add(Divisor[1] + "*/");
+                                    }
+                                    else
+                                    {
+                                        Comentarios.Add(Divisor[1] + DividirComentario[1] + "*/");
+                                    }
+
+                                }
+
+
+                            }
+                            else
+                            {
+                                Comentarios.Add(DividirComentario[0] + "*/");
+                                if (!DividirComentario[1].Trim(' ').Equals(""))
+                                {
+                                    Analisis(DividirComentario[1], ContadorLineas);
+                                }
+                            }
+                        }
+
+
+                    }
+                }
+
             }
             reader.Close();
+        }
+        public void Analisis(string linea, int x)
+        {
+
         }
 
     }
