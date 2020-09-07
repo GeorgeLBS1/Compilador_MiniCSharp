af83925be5b268a2427ce05f353b25e82094e91d
Diaz Monterroso <jj.diaz002@gmail.com>
2020-09-07 16:18:23 -0600

    Parte de las reglas de producción creadas

---
+++ Compilador_MiniCSharp/AnalizadorSintacticoRecursivo.cs
@@ -0,0 +1,381 @@
+﻿using Compilador_MiniCSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniC
+{
+    public class AnalizadorSintacticoRecursivo
+    {
+        Queue<Token> Cola_Tokens = new Queue<Token>();        
+        List<string> Lista_Types = new List<string>()
+        {
+            "int", "double", "bool", "string", "ident"
+        };
+        public AnalizadorSintacticoRecursivo(List<Token> tokens) //CONSTRUCTOR Creación de una cola con todos los tokens que se pasarán al analizador sintáctico
+        {
+            foreach (var item in tokens)
+            {
+                Cola_Tokens.Enqueue(item);
+            }
+        }
+        public void MatchToken(string expected) //Función match token
+        {
+            if (Cola_Tokens.Count > 0)
+            {
+                switch (expected)
+                {
+                    case "ident": //En caso sea un ident comparar el tipo de token
+                        if (Cola_Tokens.Peek().Tipo_token == 5) //Si el token es el que se esperaba
+                        {
+                            Cola_Tokens.Dequeue(); //Analizar el siguiente Token
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Error sintáctico en linea: {Cola_Tokens.Peek().Linea}, columnas: {Cola_Tokens.Peek().CInicio}-{Cola_Tokens.Peek().CFinal}. Se esperaba un: {expected}");
+                        }
+                        break;
+                    case "intConstant": //En caso sea un numero el esperado comparar con tipo de token
+                        if (Cola_Tokens.Peek().Tipo_token == 3) //Si el token es el que se esperaba
+                        {
+                            Cola_Tokens.Dequeue(); //Analizar el siguiente Token
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Error sintáctico en linea: {Cola_Tokens.Peek().Linea}, columnas: {Cola_Tokens.Peek().CInicio}-{Cola_Tokens.Peek().CFinal}. Se esperaba un: {expected}");
+                        }
+                        break;
+                    case "doubleConstant":
+                        if (Cola_Tokens.Peek().Tipo_token == 2) //Si el token es el que se esperaba
+                        {
+                            Cola_Tokens.Dequeue(); //Analizar el siguiente Token
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Error sintáctico en linea: {Cola_Tokens.Peek().Linea}, columnas: {Cola_Tokens.Peek().CInicio}-{Cola_Tokens.Peek().CFinal}. Se esperaba un: {expected}");
+                        }
+                        break;
+                    case "boolConstant":
+                        if (Cola_Tokens.Peek().Tipo_token == 1) //Si el token es el que se esperaba
+                        {
+                            Cola_Tokens.Dequeue(); //Analizar el siguiente Token
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Error sintáctico en linea: {Cola_Tokens.Peek().Linea}, columnas: {Cola_Tokens.Peek().CInicio}-{Cola_Tokens.Peek().CFinal}. Se esperaba un: {expected}");
+                        }
+                        break;
+                    case "stringConstant":
+                        if (Cola_Tokens.Peek().Tipo_token == 6) //Si el token es el que se esperaba
+                        {
+                            Cola_Tokens.Dequeue(); //Analizar el siguiente Token
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Error sintáctico en linea: {Cola_Tokens.Peek().Linea}, columnas: {Cola_Tokens.Peek().CInicio}-{Cola_Tokens.Peek().CFinal}. Se esperaba un: {expected}");
+                        }
+                        break;
+                    default: //En caso no sea un tipo de identificador o un constante de algun tipo comparar la palabra o caracter como tal
+                        if (Cola_Tokens.Peek().Palabra == expected) //Si el token es el que se esperaba
+                        {
+                            Cola_Tokens.Dequeue(); //Analizar el siguiente Token
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Error sintáctico en linea: {Cola_Tokens.Peek().Linea}, columnas: {Cola_Tokens.Peek().CInicio}-{Cola_Tokens.Peek().CFinal}. Se esperaba un: {expected}");
+                        }
+                        break;
+                }
+                if (expected != "ident") //En caso de que se espere algo diferente a ident
+                {
+                    
+                }
+                else
+                {
+                    
+                }
+                
+            }            
+        }
+
+
+        public void Parse_Program()
+        {
+            Parse_Decl2(); //Parsear Decl'
+        }
+        void Parse_Decl2()
+        {
+            Parse_Decl(); //Parsear Decl
+            if (Lista_Types.Contains(Cola_Tokens.Peek().Palabra) == true || Cola_Tokens.Peek().Tipo_token == 5) //Si en dado caso viene un Decl'
+            {
+                Parse_Decl2(); //Parsear Decl'
+            }
+            
+
+        }
+        void Parse_Decl()
+        {
+            if (Lista_Types.Contains(Cola_Tokens.Peek().Palabra) == true || Cola_Tokens.Peek().Tipo_token == 5) //Entrar a VariableDecl
+            {
+                Parse_VariableDecl();
+            }
+            else if(Cola_Tokens.Peek().Palabra == "void" || (Lista_Types.Contains(Cola_Tokens.Peek().Palabra) == true || Cola_Tokens.Peek().Tipo_token == 5)) //se va a analizar por el lado de FunctionDecl
+            {
+                Parse_FunctionDecl();
+            }
+        }
+        void Parse_VariableDecl()
+        {
+            Parse_Variable(); //Parsear variable
+            MatchToken(";"); //MatchToken con ";"
+            
+        }
+        void Parse_Variable()
+        {
+            Parse_Type(); //Parsear type
+            MatchToken("ident"); //MatchToken con "ident"
+        }
+        void Parse_Type()
+        {
+            Parse_Type2(); //Parsear Type'
+            if (Cola_Tokens.Peek().Palabra == "[]") //En dado caso venga []
+            {
+                MatchToken("[]");
+            }
+            //Si viene epsilon
+        }
+        void Parse_Type2() //Parse de Type'
+        {
+            if (Cola_Tokens.Peek().Tipo_token != 5) //si no es un identificador
+            {
+                switch (Cola_Tokens.Peek().Palabra)
+                {
+                    case "int":
+                        MatchToken("int"); //Match con token int
+                        break;
+                    case "double":
+                        MatchToken("double"); //Match con token double
+                        break;
+                    case "bool":
+                        MatchToken("bool"); //Match con token bool
+                        break;
+                    case "string":
+                        MatchToken("string"); //Match con token string
+                        break;
+                    default:
+                        Console.WriteLine("Error token no esperado"); //Modificar mensaje posteriormente
+                        break;
+                }
+            }
+            else
+            {
+                MatchToken("ident"); //Hacer match token con ident
+            }
+            
+        }
+
+        void Parse_FunctionDecl()
+        {
+            if (Lista_Types.Contains(Cola_Tokens.Peek().Palabra) == true || Cola_Tokens.Peek().Tipo_token == 5) //Si viene un Type
+            {
+                Parse_Type(); //Parsear Type
+                Parse_FunctionDecl2(); //Parsear functionDecl'
+            }
+            else if (Cola_Tokens.Peek().Palabra == "void") //Si viene la palabra void
+            {
+                MatchToken("void"); //Hacer matchtoken de void
+                Parse_FunctionDecl2(); //Parsear functionDecl'
+            }
+            else
+            {
+                Console.WriteLine("Error token no esperado"); //Modificar mensaje posteriormente
+            }
+        }
+        void Parse_FunctionDecl2()
+        {
+            MatchToken("ident"); //Hacer match del token ident
+            MatchToken("("); //Hacer match del token "("
+            Parse_Formals(); //Parsear Formals
+            MatchToken(")"); //Hacer match del token ")"
+            Parse_Stmt2();
+        }
+        void Parse_Formals()
+        {
+            if (Lista_Types.Contains(Cola_Tokens.Peek().Palabra) == true || Cola_Tokens.Peek().Tipo_token == 5) //En caso venga una variable '
+            {
+                Parse_Variable2(); //Parsear Variable'
+            }
+        }
+        void Parse_Variable2() //Parser de Variable'
+        {
+            Parse_Variable(); //Parsear variable
+            if (Lista_Types.Contains(Cola_Tokens.Peek().Palabra) == true || Cola_Tokens.Peek().Tipo_token == 5)//Si viene otra variable
+            {
+                Parse_Variable2(); //Parsear variable'
+            }
+            
+        }
+        void Parse_Stmt2()
+        {
+            if (Cola_Tokens.Peek().Palabra == "while" || Cola_Tokens.Peek().Palabra == "Print" || Cola_Tokens.Peek().Tipo_token == 5 || Cola_Tokens.Peek().Tipo_token ==  3 || Cola_Tokens.Peek().Tipo_token == 2 || Cola_Tokens.Peek().Tipo_token == 1 || Cola_Tokens.Peek().Tipo_token == 6 || Cola_Tokens.Peek().Palabra == "null" || Cola_Tokens.Peek().Palabra == "this" || Cola_Tokens.Peek().Palabra == "(" || Cola_Tokens.Peek().Palabra == "-" || Cola_Tokens.Peek().Palabra == "!")
+            {
+                Parse_Stmt(); //Parsear Stmt
+                Parse_Stmt2(); //Parsear Stmt'
+            }
+        }
+        void Parse_Stmt()
+        {
+            if (Cola_Tokens.Peek().Palabra == "while") //En dado caso el Stmt sea un while
+            {
+                Parse_WhileStmt(); //Parsear while
+            }
+            else if (Cola_Tokens.Peek().Palabra == "Print") //En dado caso el stmt sea un print
+            {
+                Parse_PrintStmt(); //Parsear print
+            }
+            else if (Cola_Tokens.Peek().Tipo_token == 5 || Cola_Tokens.Peek().Tipo_token == 3 || Cola_Tokens.Peek().Tipo_token == 2 || Cola_Tokens.Peek().Tipo_token == 1 || Cola_Tokens.Peek().Tipo_token == 6 || Cola_Tokens.Peek().Palabra == "null" || Cola_Tokens.Peek().Palabra == "this" || Cola_Tokens.Peek().Palabra == "(" || Cola_Tokens.Peek().Palabra == "-" || Cola_Tokens.Peek().Palabra == "!") //En dado caso sea una Expresión
+            {
+                Parse_Expr(); //Parse de Expresión
+                MatchToken(","); //Match con la coma que puede venir después de una exp
+            }
+            else
+            {
+                Console.WriteLine("Error"); //Modificar despues
+            }
+        }
+        void Parse_WhileStmt()
+        {
+            MatchToken("while"); //Match de "while"
+            MatchToken("("); //Match de "("
+            Parse_Expr(); //Parsear expresión
+            MatchToken(")");
+            Parse_Stmt(); //Parse stmt
+        }
+        void Parse_PrintStmt()
+        {
+            MatchToken("Print");
+            MatchToken("(");
+            Parse_Expre2(); //Parsear Expre'
+            MatchToken(",");
+            MatchToken(")");
+            MatchToken(";");
+        }
+        void Parse_Expre2() //Parse de Expre'
+        {
+            Parse_Expr();
+            Parse_Expre2(); 
+        }
+        void Parse_Expr() //Parse de expre
+        {
+            Parse_ExprM();
+            Parse_Expr2();
+        }
+        void Parse_Expr2() //Parser de Expr'
+        {
+            if (Cola_Tokens.Peek().Palabra == "&&") //Si viene AND
+            {
+                MatchToken("&&");
+                Parse_ExprM();
+                Parse_Expr2();
+            }
+            else if (Cola_Tokens.Peek().Palabra == "||") //Si viene OR
+            {
+                MatchToken("||");
+                Parse_ExprM();
+                Parse_Expr2();
+            }
+            else if (true) //Si viene Epsilon
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Error"); //MODIFICAR
+            }
+
+        }
+        void Parse_ExprM()
+        {
+            Parse_ExprN();
+            Parse_ExprM2();
+        }
+        void Parse_ExprM2() //Parser de ExprM'
+        {
+            if (Cola_Tokens.Peek().Palabra == "==")
+            {
+                MatchToken("==");
+                Pase_ExprN();
+                Parse_ExprM2();
+            }
+            else if (Cola_Tokens.Peek().Palabra == "!=")
+            {
+                MatchToken("!=");
+                Pase_ExprN();
+                Parse_ExprM2();
+            }
+            else if (Cola_Tokens.Peek().Palabra == ">=")
+            {
+                MatchToken(">=");
+                Pase_ExprN();
+                Parse_ExprM2();
+            }
+            else if (Cola_Tokens.Peek().Palabra == "<=")
+            {
+                MatchToken("<=");
+                Pase_ExprN();
+                Parse_ExprM2();
+            }
+            else if (Cola_Tokens.Peek().Palabra == "<")
+            {
+                MatchToken("<");
+                Pase_ExprN();
+                Parse_ExprM2();
+            }
+            else if (Cola_Tokens.Peek().Palabra == ">")
+            {
+                MatchToken(">");
+                Pase_ExprN();
+                Parse_ExprM2();
+            }
+            else if (true) //Epsilon 
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("Error"); //MODIFICAR
+            }
+        }
+
+
+
+
+        void Parse_Constant() //Parser para constantes
+        {
+            if (Cola_Tokens.Peek().Tipo_token == 3) //Caso de constante entero
+            {
+                MatchToken("intConstant");
+            }
+            else if (Cola_Tokens.Peek().Tipo_token == 2) //Constante double
+            {
+                MatchToken("doubleConstant");
+            }
+            else if (Cola_Tokens.Peek().Tipo_token == 1) //Constante bool
+            {
+                MatchToken("boolConstant");
+            }
+            else if (Cola_Tokens.Peek().Tipo_token == 6) //Constante string
+            {
+                MatchToken("stringConstant");
+            }
+            else if (Cola_Tokens.Peek().Palabra == "null") //null
+            {
+                MatchToken("null");
+            }
+            else
+            {
+                Console.WriteLine("Error"); //MODIFICAR
+            }
+        }
+    }
+}
